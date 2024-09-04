6d8544f472fad4446a8518d878434241fedb883f
Nishant ketu <nketu06@gmail.com>
2024-09-05 00:16:16 +0530

    added heapify opereation

--- heap/min_heap.py
+++ heap/min_heap.py
@@ -20,8 +20,7 @@ class MinHeap:
         self.__upheapify()
 
 
-    def __downheapify(self):
-        idx=0
+    def __downheapify(self,idx):
         while ((idx*2)+1) < self.size:
             lc,rc=(idx*2)+1,(idx*2)+2
             minimum=min(self.arr[idx],self.arr[lc])
@@ -44,19 +43,33 @@ class MinHeap:
         self.arr[0],self.arr[last_idx]=self.arr[last_idx],self.arr[0]
         self.arr.pop()
         self.size-=1
-        self.__downheapify()
+        self.__downheapify(0)
         return ans
+    
+    def heapify(self,arr):
+        self.arr=arr
+        self.size=len(self.arr)
+        # calculate last non leaf node 
+        # parent=(idx-1)//2 , here idx of last node is size - 1 and after resolving we get below last_non_leaf
+        last_non_leaf=(self.size//2)+1
+        for idx in range(last_non_leaf,-1,-1):
+            self.__downheapify(idx)
 
 
 
 
 if __name__ == '__main__':
     mh=MinHeap()
-    print(mh.arr)
-    mh.insert(10)
-    print("added 10 \n",mh.arr)
-    mh.remove()
-    print("removed min\n",mh.arr)
+    # print(mh.arr)
+    # mh.insert(10)
+    # print("added 10 \n",mh.arr)
+    # mh.remove()
+    # print("removed min\n",mh.arr)
+
+    mh.heapify([7,3,5,1,6,8,10,2,13,14,-2])
+    print("heapified array\n",mh.arr)
+
+
 
 
     
